70a01ea5bd89b0e8b3fd3865a99b2857d13c2e2f
Som Akudu <somakudu@gmail.com>
2023-02-22 17:09:02 +0000

    fix THREE not defined bug

--- js/script.js
+++ js/script.js
@@ -6,15 +6,26 @@ const canvas = document.querySelector('canvas.webgl')
 //Scene
 const scene = new THREE.Scene()
 
+//Test Cube
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "#ff0000"
+})
+const cube = new THREE.Mesh(
+  cubeGeometry, cubeMaterial
+)
+scene.add(cube)
+
 //Sizes
-const sizes {
+const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
 
 //Camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 1000)
-camera
+camera.position.z = 5
+scene.add(camera)
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -23,7 +34,7 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true
 })
 
-renderer.setSizes(sizes.width, sizes.height)
+renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 renderer.render(scene, camera)
