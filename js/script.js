2b54604aee348f3ed7baba4295df1c07cb849638
Som Akudu <somakudu@gmail.com>
2023-02-22 16:14:57 +0000

    add details for camera and window size

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@
  * Base
 */
 const canvas = document.querySelector('canvas.webgl')
+
+//Scene
+const scene = new THREE.Scene()
+
+//Sizes
+const sizes {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+//Camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 1000)
+camera
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  alpha: true
+})
+
+renderer.setSizes(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.render(scene, camera)
